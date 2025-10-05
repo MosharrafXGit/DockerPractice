baa9cc75acbf3b166688b2655b0ae08e353995be
MosharrafXGit <memosharraf@gmail.com>
2025-10-05 17:06:57 +0530

    initial commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+
+    public static void printSystemProperties() {
+        System.out.println("Printing system properties using java");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Java Program Started");
+    }
+}
